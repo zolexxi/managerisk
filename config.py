d5d24cd62a90afd992c6f8188a301a841d6c44e9
Dennis Iliaens <dennis_iliaens@hotmail.com>
2021-04-26 10:51:15 +0000

    update

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+#List of (environment) variables used for setup
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
@@ -18,10 +20,27 @@ class Config:
     def init_app(app):
         pass
 
+# Production config class
+
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
+# Docker config class
+        
+class DockerConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to stderr
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+# Dictionary with all available configs. Add new entry if you add another config class
 config = {
     'production': ProductionConfig,
+    'docker': DockerConfig,
 }
